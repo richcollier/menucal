2b741ba25532b3351b827c86a1b683acaeb3e737
rich collier <richcollier@gmail.com>
2026-03-12 14:53:49 -0400

    Initial Commit

---
+++ MenuCal/MenuCalApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuCalApp.swift
+//  MenuCal
+//
+//  Created by richcollier on 3/12/26.
+//
+
+import SwiftUI
+
+@main
+struct MenuCalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
